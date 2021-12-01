83a3ab6d362820f3c9fe0f50e9aff994fe16d185
Micah <maj180004@utdallas.edu>
2021-12-01 12:45:25 -0600

    added atk animation

---
+++ Assets/EnemyAtkHazard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAtkHazard : MonoBehaviour
+{
+    public int Damage;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerHealth())
+        {
+            other.GetComponent<PlayerHealth>().TakeDamage(Damage);
+        }
+    }
+}
