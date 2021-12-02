484aafa43d45ad41aa216f13182d00d2e09db219
Micah <maj180004@utdallas.edu>
2021-12-01 22:57:07 -0600

    added stuff

--- Assets/EnemyAtkHazard.cs
+++ Assets/EnemyAtkHazard.cs
@@ -9,10 +9,12 @@ public class EnemyAtkHazard : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        PlayerHealth health = other.GetComponent <PlayerHealth>();
+        PlayerHealth health = other.GetComponent<PlayerHealth>();
         if (health)
         {
+            
             health.TakeDamage(Damage);
+
         }
     }
 }
--- Assets/EnemyTurnHandle.cs
+++ Assets/EnemyTurnHandle.cs
@@ -6,11 +6,12 @@ public class EnemyTurnHandle : MonoBehaviour
 {
     public bool FinishedTurn;
     public int AttackAmounts;
+    [SerializeField] AudioClip atkSound = null;
 
     public void Start()
     {
         FinishedTurn = false;
-
+        Audio.PlayClip2D(atkSound, .3f);
         int atkNumb = Random.Range(0, AttackAmounts);
         GetComponent<Animator>().SetInteger("AtkDex", atkNumb);
     }
